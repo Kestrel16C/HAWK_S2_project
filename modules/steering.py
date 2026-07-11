27ec5bba309a9a6757a5bfce71d6067bb0f0fa10
Kestrel <kestrel16c@gmail.com>
2026-07-11 13:18:12 +0200

    changed steering servo slew rate limit back to 0 to disable whole block

--- modules/steering.py
+++ modules/steering.py
@@ -132,7 +132,7 @@ class Steering:
         # vor dem Ziel, kein Endanschlag-Schnappen) kombiniert mit harter
         # Geschwindigkeitsgrenze für grosse Sprünge. Zeitbasiert, funktioniert
         # daher unabhängig von der tatsächlichen Loop-Frequenz.
-        self.slew_deg_per_s = 200.0   # harte Obergrenze Winkelgeschwindigkeit
+        self.slew_deg_per_s = 0   # harte Obergrenze Winkelgeschwindigkeit
         self.smooth_per_s   = 6.0     # EMA-Gain (~Zeitkonstante 1/6 s)
         self._cur_angle     = 0.0
         self._last_slew_ms  = time.ticks_ms()
